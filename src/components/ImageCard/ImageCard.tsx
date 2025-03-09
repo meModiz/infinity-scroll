f3eb0d174a69eb45f9c56d2e8e4b2307964cedf3
Modestas A. <modestasanilionis2005@gmail.com>
2025-03-09 21:49:27 +0200

    test

--- src/components/ImageCard/ImageCard.tsx
+++ src/components/ImageCard/ImageCard.tsx
@@ -16,7 +16,7 @@ export default function ImageCard({ image }: { image: Image }) {
           ${image.src.small} 173w, 
           ${image.src.tiny} 280w
       `} // small and medium width calculations made with aspect ratio calculator (4/3)
-        sizes="(max-width: 425px) 365px, (max-width: 768px) 320px, (max-width: 1024px) 295px, 433px"
+        sizes="(max-width: 425px) 365px, (max-width: 768px) 320px, (max-width: 1024px) 295px, (max-width: 1440px) 433px, 520px"
         loading="lazy"
         alt={image.alt}
         style={{
